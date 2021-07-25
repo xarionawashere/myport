ff8476159ffe6195f3bb7bcac60a25451d8dab3e
Xariona <42253663+xarionawashere@users.noreply.github.com>
2021-07-25 17:13:51 +0300

    Update portfolio.config.js

--- portfolio.config.js
+++ portfolio.config.js
@@ -4,7 +4,7 @@ export default ({
     name: "Xariona",
     workAt: "Dream freelancer!",
     description: "Hey!, Welcome to my website i'm backend developer. I'm 19 years old now I love to ride motorcycles, especially to ride like a vagrant. Don't forget to follow me on github!",
-    pp: "https://cdn.discordapp.com/avatars/585562692132274197/c5ec6788b4bc7613c553737c9ed2a8d9.webp",
+    pp: "https://cdn.discordapp.com/avatars/585562692132274197/67f7393731d65c00f31f32621cb573a8.png",
     favArtists: new Array("emre aydın", "Linkin Park", "Ava Max", "Coldplay", "Arap Şükrü")
   } 
 }); 
